36c60b985450c4c15cfd1d6a2bdfc4cc727af399
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-14 09:43:35 -0230

    setup tailwind theme

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '47f3c18493653e124caa');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '0068fde4c9a9c9d2efe5');
