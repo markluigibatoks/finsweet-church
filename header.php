be66e302a9b9c860f5ff97e33fc37b240706ab7c
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-14 23:39:22 -0230

    fix: spacing of the header components are not the same as seen on the layout

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <body <?php body_class(); ?>>
     <div class="main-box bg-black">
       <div class="wrapper">
-        <div class="xl:px-28 py-3 flex items-center">
+        <div class="py-3 lg:py-0 flex items-center">
           <div class="xl:pr-16 pr-8">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" />
           </div>
@@ -16,21 +16,21 @@
           <nav class="hidden lg:block pl-16 border-l-2 border-l-white/10">
               <ul class="flex gap-12">
                   <li>
-                    <a class="leading-14 text-white hover:underline <?php echo is_front_page() ? 'current-menu-item' : '' ?>" href="<?php echo site_url('/'); ?>">Home</a>
+                    <a class="leading-20 uppercase text-label-2 tracking-tighter font-light opacity-90 text-white hover:underline <?php echo is_front_page() ? 'current-menu-item' : '' ?>" href="<?php echo site_url('/'); ?>">Home</a>
                   </li>
                   <li>
-                    <a class="leading-14 text-white hover:underline" href="#">About Us</a>
+                    <a class="leading-20 uppercase text-label-2 tracking-tighter font-light opacity-90 text-white hover:underline" href="#">About Us</a>
                   </li>
                   <li>
-                    <a class="leading-14 text-white hover:underline" href="#">Sermon</a>
+                    <a class="leading-20 uppercase text-label-2 tracking-tighter font-light opacity-90 text-white hover:underline" href="#">Sermon</a>
                   </li>
                   <li>
-                    <a class="leading-14 text-white hover:underline" href="#">Blog</a>
+                    <a class="leading-20 uppercase text-label-2 tracking-tighter font-light opacity-90 text-white hover:underline" href="#">Blog</a>
                   </li>
               </ul>
           </nav>
           
-          <a href="#" class="button-primary ml-auto">Contact Us</a>
+          <a href="#" class="button-nav ml-auto">Contact Us</a>
           <button type="button" class="lg:hidden cursor-pointer text-white text-2xl -mr-4 px-4 leading-8 hover:opacity-80"><i class=" fas fa-bars"></i></button>
         </div>
       </div>
