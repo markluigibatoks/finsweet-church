cc69bfc8425c23573cb4f8b5c2b987e3f7b0e63a
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-14 23:08:13 -0230

    added header

--- header.php
+++ header.php
@@ -6,8 +6,32 @@
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
-    <div class="bg-gray-300">
-      <div class="max-w-4xl mx-auto mb-5 px-4">
-        <h1 class="text-3xl py-10"><a href="<?php echo get_home_url(); ?>" class="hover:text-blue-500">Welcome To Our Header!</a></h1>
+    <div class="main-box bg-black">
+      <div class="wrapper">
+        <div class="xl:px-28 py-3 flex items-center">
+          <div class="xl:pr-16 pr-8">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" />
+          </div>
+
+          <nav class="hidden lg:block pl-16 border-l-2 border-l-white/10">
+              <ul class="flex gap-12">
+                  <li>
+                    <a class="leading-14 text-white hover:underline <?php echo is_front_page() ? 'current-menu-item' : '' ?>" href="<?php echo site_url('/'); ?>">Home</a>
+                  </li>
+                  <li>
+                    <a class="leading-14 text-white hover:underline" href="#">About Us</a>
+                  </li>
+                  <li>
+                    <a class="leading-14 text-white hover:underline" href="#">Sermon</a>
+                  </li>
+                  <li>
+                    <a class="leading-14 text-white hover:underline" href="#">Blog</a>
+                  </li>
+              </ul>
+          </nav>
+          
+          <a href="#" class="button-primary ml-auto">Contact Us</a>
+          <button type="button" class="lg:hidden cursor-pointer text-white text-2xl -mr-4 px-4 leading-8 hover:opacity-80"><i class=" fas fa-bars"></i></button>
+        </div>
       </div>
     </div>
