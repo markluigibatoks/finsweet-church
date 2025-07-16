5609a7a0b71a45d11c097f2dccecc10b3ec588d8
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-16 14:37:32 -0230

    added bottom1 and bottom2

---
+++ components/bottom1.php
@@ -0,0 +1,47 @@
+
+<div class="main-box lg:py-32 py-10 bg-grey">
+    <div class="wrapper">
+        <div class="w-full text-center">
+            <span class="text-label-2">Watch and listen</span>
+            <h2 class="lg:text-h2 md:text-h4 text-h5 lg:mt-5  uppercase">The benefits of <span class="block">joining our church</span></h2>
+        </div>
+
+        <div class="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div class="relative w-full text-center lg:text-left">
+            <div class="w-full h-full aspect-square md:aspect-151/192">
+              <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/bottom1-img1.jpg" alt="a devotee showing passion to the church" />
+            </div>
+            <div class="absolute bottom-0 left-0 w-full h-full lg:h-inherit text-white md:pt-32 pt-8 px-8 pb-8 bg-linear-to-t from-black to-transparent flex flex-col justify-end">
+              <h2 class="text-h5 uppercase">WATCH AND LISTEN TO OUR SERMONS</h2>
+            </div>
+          </div>
+
+          <div class="relative w-full text-center lg:text-left">
+            <div class="w-full h-full aspect-square md:aspect-151/192">
+              <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/bottom1-img2.jpg" alt="a devotee showing passion to the church" />
+            </div>
+            <div class="absolute bottom-0 left-0 w-full h-full lg:h-inherit text-white md:pt-32 pt-8 px-8 pb-8 bg-linear-to-t from-black to-transparent flex flex-col justify-end">
+              <h2 class="text-h5 uppercase">WATCH AND LISTEN TO OUR SERMONS</h2>
+            </div>
+          </div>
+
+          <div class="relative w-full text-center lg:text-left">
+            <div class="w-full h-full aspect-square md:aspect-151/192">
+              <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/bottom1-img3.jpg" alt="a devotee showing passion to the church" />
+            </div>
+            <div class="absolute bottom-0 left-0 w-full h-full lg:h-inherit text-white md:pt-32 pt-8 px-8 pb-8 bg-linear-to-t from-black to-transparent flex flex-col justify-end">
+              <h2 class="text-h5 uppercase">WATCH AND LISTEN TO OUR SERMONS</h2>
+            </div>
+          </div>
+
+          <div class="relative w-full text-center lg:text-left">
+            <div class="w-full h-full aspect-square md:aspect-151/192">
+              <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/bottom1-img4.jpg" alt="a devotee showing passion to the church" />
+            </div>
+            <div class="absolute bottom-0 left-0 w-full h-full lg:h-inherit text-white md:pt-32 pt-8 px-8 pb-8 bg-linear-to-t from-black to-transparent flex flex-col justify-end">
+              <h2 class="text-h5 uppercase">WATCH AND LISTEN TO OUR SERMONS</h2>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
--- front-page.php
+++ front-page.php
@@ -3,5 +3,7 @@
     get_template_part("/components/banner");
     get_template_part("/components/middle1");
     get_template_part("/components/middle2");
+    get_template_part("/components/bottom1");
+    get_template_part("/components/bottom2");
     get_footer();
 ?>
