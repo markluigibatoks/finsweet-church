8a3d26879053525a90bc80afd4e1efa8031c8b75
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-15 22:53:46 -0230

    add: middle1

---
+++ components/middle1.php
@@ -0,0 +1,34 @@
+<div class="main-box">
+    <div class="wrapper lg:pt-32 py-10">
+        <div class="w-full text-center">
+            <span class="text-label-2 uppercase">sub-headline</span>
+            <h2 class="lg:mt-4 text-h5 lg:text-h2 uppercase">a church that's relevant</h2>
+        </div>
+
+        <div class="lg:mt-16 mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div class="md:px-20 md:pb-16 md:pt-20 px-4 pt-10 pb-6 bg-light-orange border-b-16 border-b-primary w-full text-center lg:text-left">
+            <div class="bg-primary text-[#161722] w-12 h-12 display flex items-center justify-center rounded-full mx-auto lg:mx-0">
+              <img src="<?php echo get_template_directory_uri();?>/assets/images/middle1-icon1.svg" alt="icon" />
+            </div>
+            <h2 class="text-h4 mb-2 mt-6">About us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+          <div class="md:px-20 md:pb-16 md:pt-20 px-4 pt-10 pb-6 bg-light-orange border-b-16 border-b-primary w-full text-center lg:text-left">
+            <div class="bg-primary text-[#161722] w-12 h-12 display flex items-center justify-center rounded-full mx-auto lg:mx-0">
+              <img src="<?php echo get_template_directory_uri();?>/assets/images/middle1-icon2.svg" alt="icon" />
+            </div>
+            <h2 class="text-h4 mb-2 mt-6">Get involved</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        
+          <div class="md:col-span-2 lg:col-span-1 md:px-20 md:pb-16 md:pt-20 px-4 pt-10 pb-6 bg-light-orange border-b-16 border-b-primary w-full text-center lg:text-left">
+            <div class="bg-primary text-[#161722] w-12 h-12 display flex items-center justify-center rounded-full mx-auto lg:mx-0">
+              <img src="<?php echo get_template_directory_uri();?>/assets/images/middle1-icon3.svg" alt="icon" />
+            </div>
+            <h2 class="text-h4 mb-2 mt-6">Giving back</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+    </div>
+</div>
--- front-page.php
+++ front-page.php
@@ -1,5 +1,6 @@
 <?php
     get_header();
     get_template_part("/components/banner");
+    get_template_part("/components/middle1");
     get_footer();
 ?>
