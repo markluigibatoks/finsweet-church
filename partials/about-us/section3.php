4c2e2f137c7bde8f7090a5cfcd201d629773f604
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-18 16:24:16 -0230

    add: about-us/section3

---
+++ partials/about-us/section3.php
@@ -0,0 +1,113 @@
+<div class="main-box">
+    <div class="wrapper lg:pb-32 pb-10">
+        <div class="w-full text-center">
+            <span class="text-label-2">Church Members</span>
+            <h2 class="lg:text-h2 md:text-h4 text-h5 lg:mt-4">Meet Our Inspirational Team</h2>
+        </div>
+
+        <div class="lg:mt-16 mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div class="bg-grey py-12 px-8 w-full text-center">
+            <div class="max-w-[152px] mx-auto rounded-full aspect-square border-white border-8">
+              <img class="rounded-full object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section3-img1.jpg" alt="kim bowen"/>
+            </div>
+            <h2 class="mt-4 text-h5 mb-2 uppercase">Kim Bowen</h2>
+            <span class="opacity-70">Pastor, Church</span>
+            
+            <ul class="mt-4 flex justify-center items-center gap-5">
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/facebook.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/twitter.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/linkedin.svg" alt="icon"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="bg-grey py-12 px-8 w-full text-center">
+            <div class="max-w-[152px] mx-auto rounded-full aspect-square border-white border-8">
+              <img class="rounded-full object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section3-img2.jpg" alt="Danielle Watkins"/>
+            </div>
+            <h2 class="mt-4 text-h5 mb-2 uppercase">Danielle Watkins</h2>
+            <span class="opacity-70">Pastor, Church</span>
+            
+            <ul class="mt-4 flex justify-center items-center gap-5">
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/facebook.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/twitter.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/linkedin.svg" alt="icon"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="bg-grey py-12 px-8 w-full text-center">
+            <div class="max-w-[152px] mx-auto rounded-full aspect-square border-white border-8">
+              <img class="rounded-full object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section3-img3.jpg" alt="Naomi Craig"/>
+            </div>
+            <h2 class="mt-4 text-h5 mb-2 uppercase">Naomi Craig</h2>
+            <span class="opacity-70">Pastor, Church</span>
+            
+            <ul class="mt-4 flex justify-center items-center gap-5">
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/facebook.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/twitter.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/linkedin.svg" alt="icon"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="bg-grey py-12 px-8 w-full text-center">
+            <div class="max-w-[152px] mx-auto rounded-full aspect-square border-white border-8">
+              <img class="rounded-full object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section3-img4.jpg" alt="Santos Payne"/>
+            </div>
+            <h2 class="mt-4 text-h5 mb-2 uppercase">Santos Payne</h2>
+            <span class="opacity-70">Pastor, Church</span>
+            
+            <ul class="mt-4 flex justify-center items-center gap-5">
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/facebook.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/twitter.svg" alt="icon"/>
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" class="flex items-center justify-center text-black hover:opacity-70">
+                  <img src="<?php echo get_template_directory_uri();?>/assets/images/linkedin.svg" alt="icon"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </div>
+</div>
