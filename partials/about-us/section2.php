ae7ada0c84a93aa680754b17ee4ba666b6580504
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-18 15:42:08 -0230

    add: about-us/section3

---
+++ partials/about-us/section2.php
@@ -0,0 +1,46 @@
+<div class="main-box bg-white lg:pb-32 pb-10">
+    <div class="wrapper">
+        <div class="w-full text-center">
+            <span class="uppercase">Benefits</span>
+            <h2 class="uppercase lg:text-h2 md:text-h4 text-h5 lg:mt-4">The Benefits Of Joining Our Church</h2>
+        </div>
+
+        <div class="lg:mt-16 mt-5 grid grid-cols-1 md:grid-cols-2 lg:gap-y-24 lg:gap-x-6 gap-5">
+          <div class="md:order-1 order-2 flex flex-col justify-center w-full text-center md:text-left">
+            <h2 class="md:text-h4 text-h5 lg:mb-4 uppercase">Find Fulfillment And Joy</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+          </div>
+        
+          <div class="order-1 md:order-2 w-full h-full max-w-max mx-auto aspect-25/16">
+            <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img1.jpg" alt="a man who finds fulfullment and joy"/>
+          </div>
+
+          <div class="order-3 lg:order-3 w-full h-full max-w-max mx-auto aspect-25/16">
+            <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img2.jpg" alt="someone reading the holy bible"/>
+          </div>
+        
+          <div class="order-4 flex flex-col justify-center w-full text-center md:text-left">
+            <h2 class="md:text-h4 text-h5 lg:mb-4 uppercase">Shared Values</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+          </div>
+
+          <div class="md:order-5 order-6 flex flex-col justify-center w-full text-center md:text-left">
+            <h2 class="md:text-h4 text-h5 lg:mb-4 uppercase">Charity Events</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+          </div>
+        
+          <div class="order-5 md:order-6 w-full h-full max-w-max mx-auto aspect-25/16">
+            <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img3.jpg" alt="both men and women are putting their hands up in the sky"/>
+          </div>
+
+          <div class="order-7 w-full h-full max-w-max mx-auto aspect-25/16">
+            <img class="object-cover w-full h-full" src="<?php echo get_template_directory_uri();?>/assets/images/about-us/section2-img4.jpg" alt="an family image mostly found in church"/>
+          </div>
+        
+          <div class="order-8 flex flex-col justify-center w-full text-center md:text-left">
+            <h2 class="md:text-h4 text-h5 lg:mb-4 uppercase">All Are Welcome</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+          </div>
+        </div>
+    </div>
+</div>
