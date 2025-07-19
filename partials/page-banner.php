7bed0f6ffd7012651f7df20c84960754c7ec4d1f
Mark-Luigi <markluigibatoks@gmail.com>
2025-07-19 11:50:24 -0230

    add: background to page_banner info

--- partials/page-banner.php
+++ partials/page-banner.php
@@ -1,9 +1,9 @@
 <div class="main-box p-0 w-full h-full relative">
   <img class="object-cover w-full h-full" src="<?php echo $args['image']['src']; ?>" alt="<?php echo $args['image']['alt'] ?>"/>
 
-  <div class="main-box lg:absolute lg:left-0 lg:top-0 lg:h-full">
+  <div class="main-box py-10 bg-black text-white lg:text-black lg:bg-transparent lg:absolute lg:left-0 lg:top-0 lg:h-full">
       <div class="wrapper lg:flex lg:items-center lg:h-full">
-          <div>
+          <div class="lg:text-left text-center">
             <span class="uppercase font-bold"><?php echo get_the_title(); ?></span>
             <h2 class="lg:mt-4 uppercase align-top lg:text-h2 md:text-h4 text-h5"><?php echo $args['heading']; ?></h2>
           </div>
